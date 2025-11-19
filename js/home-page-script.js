556c157c74c3764afd219fc8ffedc558125e38f4
Lorenzo Raffaeli <lorenzoraffaeli8@gmail.com>
2025-11-19 16:38:59 +0100

    added main div

--- js/home-page-script.js
+++ js/home-page-script.js
@@ -6,6 +6,14 @@ function handleLoginRedirect()
     {
         window.location.href = "./pages/login-page.html";
     }
+
+    const loginButton2 = document.getElementById("login-button2");
+
+    loginButton2.onclick = function()
+    {
+        window.location.href = "./pages/login-page.html";
+    }
 }
 
 window.onload = handleLoginRedirect;
+
