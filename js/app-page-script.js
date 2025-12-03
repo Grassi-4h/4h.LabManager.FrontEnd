15d7d4ccac2dc7fafb435c11e6d5c2562bbf33cd
Lorenzo Raffaeli <lorenzoraffaeli8@gmail.com>
2025-12-03 16:30:28 +0100

    creating button function

---
+++ js/app-page-script.js
@@ -0,0 +1,57 @@
+function ripristinaStatoIniziale() {
+    const contenutoCentrale = document.getElementById('contenutoCentrale');
+    const titoloContenuto = '<b>Sistema gestionale</b>';
+
+    contenutoCentrale.classList.remove(
+        'bg-white', 
+        'text-black', 
+        'shadow-xl'
+    );
+
+    contenutoCentrale.classList.add(
+        'bg-white/10', 
+        'backdrop-blur-xs', 
+        'border', 
+        'border-white/40', 
+        'shadow-[0_8px_20px_0_rgba(0,0,0,0.65)]'
+    );
+
+    contenutoCentrale.innerHTML = titoloContenuto;
+}
+
+function selectFirstOption() {
+
+    const bottoneOpzione1 = document.getElementById('btnOpzione1');
+    const contenutoCentrale = document.getElementById('contenutoCentrale');
+    const logo = document.getElementById('logo-app-header')
+
+        bottoneOpzione1.onclick = function() {
+
+            contenutoCentrale.classList.remove(
+
+                'bg-white/10', 
+                'backdrop-blur-xs', 
+                'border', 
+                'border-white/40', 
+                'shadow-[0_8px_20px_0_rgba(0,0,0,0.65)]'
+
+            );
+
+            contenutoCentrale.classList.add(
+
+                'bg-white', 
+                'text-black', 
+                'shadow-xl'
+
+            ); 
+
+            contenutoCentrale.innerHTML = '';
+        }
+
+        logo.onclick = ripristinaStatoIniziale;
+
+        logo.style.cursor = 'pointer';
+
+}
+
+document.addEventListener('DOMContentLoaded', selectFirstOption);
